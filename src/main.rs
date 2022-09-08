17b4221b9e666320f01fb3bd610553eadee206e9
aaronmck <aaronatwpi@gmail.com>
2022-09-08 09:26:15 -0400

    check for file status better

--- src/main.rs
+++ src/main.rs
@@ -174,8 +174,13 @@ fn main() {
     let read_iterator = ReadIterator::new(parameters.read1, parameters.read2,parameters.index1,parameters.index2);
 
     if read_layout.has_umi() {
+        let mut cnt = 0;
         let mut read_mapping = HashMap::new();
         for rd in read_iterator {
+            cnt += 1;
+            if cnt % 10000 == 0 {
+                println!("Count {}",cnt);
+            }
             let transformed_reads = transform(rd, &read_layout);
             let first_hit = transformed_reads.get_unique_sequences().unwrap()[0].clone();
             if known_list.as_ref().is_some() {
