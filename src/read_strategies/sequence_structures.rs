80cb34ed1bca0d55af10ad2bf635e4d8e00c3da2
aaronmck <aaronatwpi@gmail.com>
2022-09-15 22:16:08 -0400

    protect against weird file issues:

--- src/read_strategies/sequence_structures.rs
+++ src/read_strategies/sequence_structures.rs
@@ -11,6 +11,7 @@ use std::io;
 use flate2::read::GzEncoder;
 use std::borrow::BorrowMut;
 use bgzip::BGZFReader;
+use std::collections::HashMap;
 
 pub struct ReadSetContainer {
     pub read_one: Record,
@@ -82,11 +83,22 @@ pub struct ReadFileContainer {
     pub index_two: PathBuf,
 }
 
+pub enum ReadPattern {
+    READ1, READ2, INDEX1, INDEX2
+}
+
+
 pub struct ReadIterator {
     pub read_one: Records<BufReader<BGZFReader<BufReader<File>>>>,
     pub read_two: Option<Records<BufReader<BGZFReader<BufReader<File>>>>>,
     pub index_one: Option<Records<BufReader<BGZFReader<BufReader<File>>>>>,
     pub index_two: Option<Records<BufReader<BGZFReader<BufReader<File>>>>>,
+
+    pub reads_processed: usize,
+    pub read2: bool,
+    pub index1: bool,
+    pub index2: bool,
+    pub broken_reads: usize,
 }
 
 pub fn unwrap_read(read: &mut Option<Records<BufReader<BGZFReader<BufReader<File>>>>>) -> Option<Record> {
@@ -111,14 +123,15 @@ pub fn unwrap_read(read: &mut Option<Records<BufReader<BGZFReader<BufReader<File
 impl Iterator for ReadIterator {
     type Item = ReadSetContainer;
 
+
     fn next(&mut self) -> Option<ReadSetContainer> {
         let next_read_one = self.read_one.next();
         if next_read_one.is_some() {
-            match next_read_one.unwrap() {
+            let ret = match next_read_one.unwrap() {
                 Ok(v) => {
                     Some(ReadSetContainer {
                         read_one: v,
-                        read_two: unwrap_read(&mut self.read_two),
+                        read_two:  unwrap_read(&mut self.read_two),
                         index_one: unwrap_read(&mut self.index_one),
                         index_two: unwrap_read(&mut self.index_two),
                     })
@@ -127,6 +140,24 @@ impl Iterator for ReadIterator {
                     println!("error parsing header: {e:?}");
                     None
                 }
+            };
+            match ret {
+                Some(x) => {
+                    if self.read2 && !self.read_two.is_some() {
+                        self.broken_reads += 1;
+                        None
+                    } else if self.index1 && !self.index_one.is_some() {
+                        self.broken_reads += 1;
+                        None
+                    } else if self.index2 && !self.index_two.is_some() {
+                        self.broken_reads += 1;
+                        None
+                    } else {
+                        self.reads_processed += 1;
+                        Some(x)
+                    }
+                }
+                None => None
             }
         } else {
             None
@@ -145,11 +176,25 @@ impl ReadIterator
     ) -> ReadIterator  {
         let r_one = ReadIterator::open_reader(&Some(read_1));
         if !r_one.is_some() {panic!("Unable to open input file");}
+
+        let read2 = ReadIterator::open_reader(&Some(read_2));
+        let index1 = ReadIterator::open_reader(&Some(index_1));
+        let index2 = ReadIterator::open_reader(&Some(index_2));
+
+        let read2_active = read2.is_some();
+        let index1_active = index1.is_some();
+        let index2_active = index2.is_some();
+
         ReadIterator {
             read_one: r_one.unwrap(),
-            read_two: ReadIterator::open_reader(&Some(read_2)),
-            index_one: ReadIterator::open_reader(&Some(index_1)),
-            index_two: ReadIterator::open_reader(&Some(index_2)),
+            read_two: read2,
+            index_one: index1,
+            index_two: index2,
+            reads_processed: 0,
+            read2: read2_active,
+            index1: index1_active,
+            index2: index2_active,
+            broken_reads: 0
         }
     }
 
@@ -164,11 +209,24 @@ impl ReadIterator
         let path3 = if read_sorter.file_3.is_some() {Some(read_sorter.file_3.as_ref().unwrap().as_path())} else {None};
         let path4 = if read_sorter.file_4.is_some() {Some(read_sorter.file_4.as_ref().unwrap().as_path())} else {None};
 
+        let read2 = ReadIterator::open_reader(&path2);
+        let index1 = ReadIterator::open_reader(&path3);
+        let index2 = ReadIterator::open_reader(&path4);
+
+        let read2_active = read2.is_some();
+        let index1_active = index1.is_some();
+        let index2_active = index2.is_some();
+
         ReadIterator {
             read_one: ReadIterator::open_reader(&Some(&path1)).unwrap(),
-            read_two: ReadIterator::open_reader(&path2),
-            index_one: ReadIterator::open_reader(&path3),
-            index_two: ReadIterator::open_reader(&path4)
+            read_two: read2,
+            index_one: index1,
+            index_two: index2,
+            reads_processed: 0,
+            read2: read2_active,
+            index1: index1_active,
+            index2: index2_active,
+            broken_reads: 0
         }
     }
 
