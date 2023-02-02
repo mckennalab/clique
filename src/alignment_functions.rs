a968afa89e1157f5d54f8fabffce0dfdb88f8d31
aaronmck <aaronatwpi@gmail.com>
2023-02-02 00:13:31 -0500

    fix the degenerate bases code

--- src/alignment_functions.rs
+++ src/alignment_functions.rs
@@ -48,9 +48,9 @@ pub fn align_reads(use_capture_sequences: &bool,
     let my_score = InversionScoring {
         match_score: 9.0,
         mismatch_score: -21.0,
-        special_character_score: 9.0,
+        special_character_score: 8.0,
         gap_open: -25.0,
-        gap_extend: -5.0,
+        gap_extend: -1.0,
         inversion_penalty: -40.0,
         min_inversion_length: 20,
     };
@@ -58,9 +58,9 @@ pub fn align_reads(use_capture_sequences: &bool,
     let my_aff_score = AffineScoring {
         match_score: 10.0,
         mismatch_score: -11.0,
-        special_character_score: 10.0,
+        special_character_score: 9.0,
         gap_open: -20.0,
-        gap_extend: -5.0,
+        gap_extend: -1.0,
         final_gap_multiplier: 1.0,
 
     };
