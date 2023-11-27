d51cac1519a46cae3a7be81d0c3aadb4a0adbdff
aaronmck <aaronatwpi@gmail.com>
2023-11-27 10:28:07 -0500

    relax matrix size check when reusing matrix

--- rust_cmd/src/alignment/alignment_matrix.rs
+++ rust_cmd/src/alignment/alignment_matrix.rs
@@ -253,8 +253,9 @@ pub fn perform_affine_alignment(alignment: &mut Alignment<Ix3>,
                                 sequence2: &Vec<FastaBase>,
                                 scoring_function: &dyn AffineScoringFunction) {
     assert!(alignment.scores.shape()[2] == 3);
-    //assert!(alignment.scores.shape()[0] >= sequence1.len() + 1);
-    //assert!(alignment.scores.shape()[1] >= sequence2.len() + 1);
+    assert!(alignment.scores.shape()[0] >= sequence1.len() + 1, "Asked to align sequence 1 with length {} in a matrix sized {} in that dimension", alignment.scores.shape()[0], sequence1.len() + 1);
+    assert!(alignment.scores.shape()[1] >= sequence2.len() + 1, "Asked to align sequence 1 with length {} in a matrix sized {} in that dimension", alignment.scores.shape()[1], sequence2.len() + 1);
+
 
     alignment.scores[[0, 0, 0]] = 0.0;
     alignment.scores[[0, 0, 1]] = MAX_NEG_SCORE;
