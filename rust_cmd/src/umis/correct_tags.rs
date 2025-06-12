a1125dc4c82be08684b1fd828be9289faacabf90
aaronmck <aaronatwpi@gmail.com>
2025-06-12 12:40:25 -0400

    sort tags; bug where if they're out of order in the reference they come in out-of-order

--- rust_cmd/src/umis/correct_tags.rs
+++ rust_cmd/src/umis/correct_tags.rs
@@ -58,6 +58,10 @@ impl SequenceCorrector {
 
         let key_value = item.ordered_unsorted_keys.pop_front().unwrap();
         if!(key_value.0 != self.tag.symbol) {
+            println!("Failed read: {}\n{}\n{}\n{:?}\n{:?}\n{:?}",
+                     &item.aligned_read.read_name,
+                     u8s(&item.aligned_read.read_aligned),
+                     u8s(&item.aligned_read.reference_aligned),&self.tag,key_value,item.ordered_unsorted_keys);
             println!("Failed read: {}\n{}\n{}",&item.aligned_read.read_name,u8s(&item.aligned_read.read_aligned),u8s(&item.aligned_read.reference_aligned));
             panic!("unable to process read");
         }
